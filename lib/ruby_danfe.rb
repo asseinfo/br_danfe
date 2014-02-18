6e8ca74ba222027c7cac13ae8e046ad8fc0b1b66
Fabricio <fabricio@asseinfo.com.br>
2014-02-18 14:00:05 -0300

    Add logotipo do DANFE header

--- lib/ruby_danfe.rb
+++ lib/ruby_danfe.rb
@@ -4,5 +4,7 @@ require "barby"
 require "barby/barcode/code_128"
 require "barby/outputter/prawn_outputter"
 require "nokogiri"
+require "yaml"
+require "ostruct"
 
 Dir[File.dirname(__FILE__) + "/**/*.rb"].each { |f| require f }
--- lib/ruby_danfe/ruby_danfe.rb
+++ lib/ruby_danfe/ruby_danfe.rb
@@ -1,21 +1,37 @@
 module RubyDanfe
-  def self.generate(pdf_filename, xml_filename, type = :danfe)
+  def self.generate(pdf_filename, xml_filename, type = :danfe, new_options = {})
+    self.options = new_options if !new_options.empty?
+
     xml_string = File.new(xml_filename)
     render_file(pdf_filename, xml_string, type)
   end
 
-  def self.render(xml_string, type = :danfe)
+  def self.render(xml_string, type = :danfe, new_options = {})
+    self.options = new_options if !new_options.empty?
+
     pdf = generatePDF(xml_string, type)
     pdf.render
   end
 
-  def self.render_file(pdf_filename, xml_string, type = :danfe)
+  def self.render_file(pdf_filename, xml_string, type = :danfe, new_options = {})
+   self.options = new_options if !new_options.empty?
+
     pdf = generatePDF(xml_string, type)
     pdf.render_file pdf_filename
   end
 
+  def self.options
+    @options ||= RubyDanfe::Options.new
+  end
+
+  def self.options=(new_options = {})
+    @options = RubyDanfe::Options.new(new_options)
+  end
+
   private
-  def self.generatePDF(xml_string, type = :danfe)
+  def self.generatePDF(xml_string, type = :danfe, new_options = {})
+    self.options = new_options if !new_options.empty?
+
     xml = XML.new(xml_string)
 
     if type == :danfe
