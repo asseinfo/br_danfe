e332bd6cc80a54dc0fe6585d659feb85c6f6ebed
Alan Costa Facchini <costafacchini@gmail.com>
2017-02-10 16:58:37 -0200

    Resize fields in the product grid.

--- lib/br_danfe/danfe_lib/det_body.rb
+++ lib/br_danfe/danfe_lib/det_body.rb
@@ -12,7 +12,7 @@ module BrDanfe
           cell_style: { padding: 2, border_width: 0 }
         }
 
-        @pdf.font_size(6) do
+        @pdf.font_size(6.5) do
           @pdf.bounding_box [0.75.cm, Helper.invert(19.59.cm)], width: 19.57.cm, height: 6.07.cm do
             @pdf.table products, options do |table|
               table.column(6..13).style(align: :right)
@@ -54,20 +54,20 @@ module BrDanfe
 
       def column_widths
         {
-          0 => 1.50.cm,
-          1 => 4.40.cm,
-          2 => 1.30.cm,
-          3 => 0.80.cm,
-          4 => 1.00.cm,
-          5 => 1.00.cm,
-          6 => 1.30.cm,
-          7 => 1.50.cm,
-          8 => 1.50.cm,
-          9 => 1.50.cm,
+          0 => 1.60.cm,
+          1 => 6.10.cm,
+          2 => 1.05.cm,
+          3 => 0.60.cm,
+          4 => 0.65.cm,
+          5 => 1.10.cm,
+          6 => 1.20.cm,
+          7 => 1.20.cm,
+          8 => 1.20.cm,
+          9 => 1.20.cm,
           10 => 1.00.cm,
           11 => 1.00.cm,
           12 => 0.90.cm,
-          13 => 0.86.cm
+          13 => 0.76.cm
         }
       end
     end
