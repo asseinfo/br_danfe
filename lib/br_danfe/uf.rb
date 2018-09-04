22db976cb889cb0fdf011ced9db4e5c403e9a8d6
Linielson Rosa <linielsonrosa@gmail.com>
2018-09-04 15:33:19 -0300

    Fix codeclimate

--- lib/br_danfe/uf.rb
+++ lib/br_danfe/uf.rb
@@ -4,9 +4,9 @@ class Uf
   end
 
   def self.all
-    [
-      :AC, :AL, :AP, :AM, :BA, :CE, :DF, :ES, :GO, :MA, :MT, :MS, :MG, :PA, :PB, :PR, :PE, :PI, :RJ, :RN, :RS, :RO, :RR,
-      :SC, :SP, :SE, :TO
+    %i[
+      AC AL AP AM BA CE DF ES GO MA MT MS MG PA PB PR PE PI RJ RN RS RO RR SC
+      SP SE TO
     ]
   end
   private_class_method :all
