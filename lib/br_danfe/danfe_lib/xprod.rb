add35e2472c0e4fcf6256ecaf59b5f8a8e8dc297
Jean Carlos Farias <contato@jeancf.com.br>
2018-09-05 10:58:10 -0300

    Fix codeclimate

--- lib/br_danfe/danfe_lib/xprod.rb
+++ lib/br_danfe/danfe_lib/xprod.rb
@@ -9,9 +9,9 @@ module BrDanfe
         xprod = "#{@det.css('prod/xProd').text}"
 
         xprod += infAdProd if has_infAdProd?
-        xprod += fci if has_fci?
-        xprod += st if has_st?
-        xprod += fcp if has_fcp?
+        xprod += fci if fci?
+        xprod += st if st?
+        xprod += fcp if fcp?
 
         xprod
       end
@@ -19,7 +19,7 @@ module BrDanfe
       private
 
       def infAdProd
-        "\n" + @det.css("infAdProd").text
+        "\n#{@det.css('infAdProd').text}"
       end
 
       def has_infAdProd?
@@ -27,32 +27,33 @@ module BrDanfe
       end
 
       def fci
-        "\n" + I18n.t("danfe.det.prod.xProdFCI", nFCI: @det.css('prod/nFCI').text)
+        "\n#{I18n.t('danfe.det.prod.xProdFCI',
+          nFCI: @det.css('prod/nFCI').text)}"
       end
 
-      def has_fci?
-        !@det.css("prod/nFCI").text.empty?
+      def fci?
+        !@det.css('prod/nFCI').text.empty?
       end
 
       def st
-        "\n" + I18n.t("danfe.det.prod.xProdST",
+        "\n#{I18n.t('danfe.det.prod.xProdST',
           pMVAST: Helper.numerify(@det.css('ICMS/*/pMVAST').text),
           pICMSST: Helper.numerify(@det.css('ICMS/*/pICMSST').text),
           vBCST: Helper.numerify(@det.css('ICMS/*/vBCST').text),
-          vICMSST: Helper.numerify(@det.css('ICMS/*/vICMSST').text))
+          vICMSST: Helper.numerify(@det.css('ICMS/*/vICMSST').text))}"
       end
 
-      def has_st?
+      def st?
         @det.css("ICMS/*/vBCST").text.to_i > 0
       end
 
       def fcp
-        "\n" + I18n.t("danfe.det.prod.xProdFCP",
-          vFCP: Helper.numerify(@det.css("ICMS00/vFCP").text),
-          pFCP: Helper.numerify(@det.css("ICMS00/pFCP").text))
+        "\n#{I18n.t('danfe.det.prod.xProdFCP',
+          vFCP: Helper.numerify(@det.css('ICMS00/vFCP').text),
+          pFCP: Helper.numerify(@det.css('ICMS00/pFCP').text))}"
       end
 
-      def has_fcp?
+      def fcp?
         @det.css("ICMS00/vFCP").text.to_i > 0
       end
     end
