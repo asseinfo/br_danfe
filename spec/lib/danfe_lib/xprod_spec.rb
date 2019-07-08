c90f666567b7866deb672c795e60b00141d9c92d
Matheus Poli <matheuspolicamilo@gmail.com>
2019-07-08 14:03:38 -0300

    Add ICMS ST Retido in DANFE item (#33)

--- spec/lib/danfe_lib/xprod_spec.rb
+++ spec/lib/danfe_lib/xprod_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 describe BrDanfe::DanfeLib::Xprod do
   describe "#render" do
-    context "when have FCI" do
+    context "when has FCI" do
       let(:xml_fci) do
         xml = <<-eos
         <det nItem="1">
@@ -23,14 +23,101 @@ describe BrDanfe::DanfeLib::Xprod do
       it "returns product + FCI" do
         expected = "MONITOR DE ARCO ELETRICO"
         expected += "\n"
-        expected +="FCI: 12232531-74B2-4FDD-87A6-CF0AD3E55386"
+        expected += "FCI: 12232531-74B2-4FDD-87A6-CF0AD3E55386"
         expect(subject.render).to eq expected
       end
     end
 
-    context "when have ST" do
-      let(:xml_st) do
-        xml = <<-eos
+    context "when has ST" do
+      context "when is Retido" do
+        let(:vBCSTRet) { 0.00 }
+        let(:pST) { 0.00 }
+        let(:vICMSSTRet) { 0.00 }
+        let(:xml) do
+          xml = <<-eos
+            <det nItem="1">
+              <prod>
+                <xProd>MONITOR DE ARCO ELETRICO</xProd>
+              </prod>
+              <imposto>
+                <ICMS>
+                  <ICMSST>
+                    <orig>0</orig>
+                    <CST>60</CST>
+                    <pST>#{pST}</pST>
+                    <vBCSTRet>#{vBCSTRet}</vBCSTRet>
+                    <vICMSSTRet>#{vICMSSTRet}</vICMSSTRet>
+                    <vBCSTDest>0.00</vBCSTDest>
+                    <vICMSSTDest>0.00</vICMSSTDest>
+                  </ICMSST>
+                </ICMS>
+                <PIS>
+                  <PISNT>
+                    <CST>04</CST>
+                  </PISNT>
+                </PIS>
+                <COFINS>
+                  <COFINSNT>
+                    <CST>04</CST>
+                  </COFINSNT>
+                </COFINS>
+              </imposto>
+            </det>
+          eos
+
+          Nokogiri::XML(xml)
+        end
+
+        subject { BrDanfe::DanfeLib::Xprod.new(xml) }
+
+        context "when has vBCSTRet" do
+          let(:vBCSTRet) { 50.00 }
+
+          it "returns product + ST Retido" do
+            expected = "MONITOR DE ARCO ELETRICO"
+            expected += "\n"
+            expected += "ST Retido: Base: 50,00 * Alíq: 0,00% * Vlr: 0,00"
+
+            expect(subject.render).to eq expected
+          end
+        end
+
+        context "when has vICMSSTRet" do
+          let(:vICMSSTRet) { 50.00 }
+
+          it "returns product + ST Retido" do
+            expected = "MONITOR DE ARCO ELETRICO"
+            expected += "\n"
+            expected += "ST Retido: Base: 0,00 * Alíq: 0,00% * Vlr: 50,00"
+
+            expect(subject.render).to eq expected
+          end
+        end
+
+        context "when has pST" do
+          let(:pST) { 50.00 }
+
+          it "returns product + ST Retido" do
+            expected = "MONITOR DE ARCO ELETRICO"
+            expected += "\n"
+            expected += "ST Retido: Base: 0,00 * Alíq: 50,00% * Vlr: 0,00"
+
+            expect(subject.render).to eq expected
+          end
+        end
+
+        context "when doesn't have any fields" do
+          it "returns product" do
+            expected = "MONITOR DE ARCO ELETRICO"
+
+            expect(subject.render).to eq expected
+          end
+        end
+      end
+
+      context "when isn't Retido" do
+        let(:xml_st) do
+          xml = <<-eos
         <det nItem="1">
           <prod>
             <xProd>MONITOR DE ARCO ELETRICO</xProd>
@@ -47,23 +134,24 @@ describe BrDanfe::DanfeLib::Xprod do
             </ICMS>
           </imposto>
         </det>
-        eos
+          eos
 
-        Nokogiri::XML(xml)
-      end
+          Nokogiri::XML(xml)
+        end
 
-      subject { BrDanfe::DanfeLib::Xprod.new(xml_st) }
+        subject { BrDanfe::DanfeLib::Xprod.new(xml_st) }
 
-      it "returns product + ST" do
-        expected = "MONITOR DE ARCO ELETRICO"
-        expected += "\n"
-        expected += "ST: MVA: 56,00% * Alíq: 17,00% * BC: 479,82 * Vlr: 29,28"
+        it "returns product + ST" do
+          expected = "MONITOR DE ARCO ELETRICO"
+          expected += "\n"
+          expected += "ST: MVA: 56,00% * Alíq: 17,00% * BC: 479,82 * Vlr: 29,28"
 
-        expect(subject.render).to eq expected
+          expect(subject.render).to eq expected
+        end
       end
     end
 
-    context "when have infAdProd" do
+    context "when has infAdProd" do
       let(:xml_infAdProd) do
         xml = <<-eos
         <det nItem="1">
@@ -88,7 +176,7 @@ describe BrDanfe::DanfeLib::Xprod do
       end
     end
 
-    context "when have FCP on ICMS00 tag" do
+    context "when has FCP on ICMS00 tag" do
       let(:xml_fcp) do
         xml = <<-eos
         <det nItem="1">
@@ -121,7 +209,7 @@ describe BrDanfe::DanfeLib::Xprod do
       end
     end
 
-    context "when have FCI + ST + infAdProd + FCP" do
+    context "when has FCI + ST + infAdProd + FCP" do
       let(:xml_IFC_ST_infAdProd) do
         xml = <<-eos
         <det nItem="1">
@@ -158,7 +246,7 @@ describe BrDanfe::DanfeLib::Xprod do
         expected += "\n"
         expected += "Informações adicionais do produto"
         expected += "\n"
-        expected +="FCI: 12232531-74B2-4FDD-87A6-CF0AD3E55386"
+        expected += "FCI: 12232531-74B2-4FDD-87A6-CF0AD3E55386"
         expected += "\n"
         expected += "ST: MVA: 56,00% * Alíq: 17,00% * BC: 479,82 * Vlr: 29,28"
         expected += "\n"
@@ -202,7 +290,7 @@ describe BrDanfe::DanfeLib::Xprod do
       end
     end
 
-    context "when have FCP ST" do
+    context "when has FCP ST" do
       let(:xml_fcp) do
         xml = <<-eos
         <det nItem="1">
