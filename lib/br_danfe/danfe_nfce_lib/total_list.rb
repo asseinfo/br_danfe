d73af8f3f00929aed5c56e60e6402972b3d6fd8b
Altair J. Fernandes <tatosjb@gmail.com>
2020-03-24 17:03:01 -0300

    start payments

--- lib/br_danfe/danfe_nfce_lib/total_list.rb
+++ lib/br_danfe/danfe_nfce_lib/total_list.rb
@@ -1,6 +1,8 @@
 module BrDanfe
   module DanfeNfceLib
     class TotalList
+      require 'bigdecimal'
+
       LINE_HEIGHT = 1.35
 
       def initialize(pdf, xml)
@@ -24,6 +26,23 @@ module BrDanfe
         @pdf.y -= 0.30.cm
         @pdf.ibox LINE_HEIGHT, 7.4, 0, @pdf.cursor, '', 'Valor Total R$', { size: 7, align: :left, border: 0, style: :bold }
         @pdf.inumeric LINE_HEIGHT, 7.4, 0, @pdf.cursor, @xml['ICMSTot > vNF'], { size: 7, align: :right, border: 0 }
+
+        @pdf.y -= 0.30.cm
+        @pdf.ibox LINE_HEIGHT, 7.4, 0, @pdf.cursor, '', 'Forma de pagamento', { size: 7, align: :left, border: 0 }
+
+        # { money: '01', check: '02', credit_card: '03', debit_card: '04', credit: '05', without_payment: '90', others: '99' }
+        payments = {}
+        @xml.css('detPag').each do |detPag|
+          # byebug
+          payments[detPag.css('tPag').text] ||= BigDecimal('0')
+          actual_payment_value = BigDecimal(detPag.css('vPag').text)
+
+          payments[detPag.css('tPag').text] += actual_payment_value
+        end
+
+        p "payments ==> #{payments["05"].to_f}"
+        # payments.
+        # @pdf.inumeric LINE_HEIGHT, 7.4, 0, @pdf.cursor, @xml['ICMSTot > vNF'], { size: 7, align: :right, border: 0 }
       end
     end
   end
