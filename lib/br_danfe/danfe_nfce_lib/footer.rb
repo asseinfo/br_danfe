91a70fa45cae563ed00e1bc9cde637afce9da816
Linielson Rosa <linielsonrosa@gmail.com>
2020-04-01 13:08:20 -0300

    Add additional information at footer to NFC-e (#58)

--- lib/br_danfe/danfe_nfce_lib/footer.rb
+++ lib/br_danfe/danfe_nfce_lib/footer.rb
@@ -6,9 +6,14 @@ module BrDanfe
         @xml = xml
       end
 
-      def render
+      def render(info = '')
         tot_trib = @xml['ICMSTot/vTotTrib'].present? ? BrDanfe::Helper.numerify(@xml['ICMSTot/vTotTrib']) : '0,00'
         @pdf.text "Tributos Totais Incidentes (Lei Federal 12.741/2012): R$ #{tot_trib}", size: 5, align: :center
+
+        if info.present?
+          @pdf.render_blank_line
+          @pdf.text info, size: 5, align: :center
+        end
       end
     end
   end
