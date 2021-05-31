5b6470ff7fe6b6bee9bd1e24adba63d0524af553
alicehabitzreuter <habitzreuter21@gmail.com>
2021-05-31 15:13:50 -0300

    fix danfe and add some tests

--- lib/br_danfe/danfe.rb
+++ lib/br_danfe/danfe.rb
@@ -3,18 +3,21 @@ module BrDanfe
     def self.new(xmls)
       xmls = [xmls] unless xmls.is_a?(Array)
 
-      parsed_xmls = []
-      xmls.each do |xml|
-        parsed_xmls << BrDanfe::XML.new(xml)
-      end
+      parsed_xmls = xmls.map { |xml| BrDanfe::XML.new(xml) }
 
       create_danfe(parsed_xmls)
     end
 
     def self.create_danfe(xmls)
-      nfce_code = '65'
-      xmls.first['ide > mod'] == nfce_code ? DanfeLib::Nfce.new(xmls) : DanfeLib::Nfe.new(xmls)
+      nfe?(xmls) ? DanfeLib::Nfe.new(xmls) : DanfeLib::Nfce.new(xmls)
     end
     private_class_method :create_danfe
+
+    def self.nfe?(xmls)
+      nfe_code = '55'
+
+      xmls.first['ide > mod'] == nfe_code
+    end
+    private_class_method :nfe?
   end
 end
