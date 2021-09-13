520486fee8797f0ec729421b167eebc4ccc1f77e
alicehabitzreuter <habitzreuter21@gmail.com>
2021-09-13 15:55:54 -0300

    Fix ProductList [skip ci]

--- lib/br_danfe/danfe_lib/nfce_lib/product_list.rb
+++ lib/br_danfe/danfe_lib/nfce_lib/product_list.rb
@@ -14,19 +14,54 @@ module BrDanfe
 
         private
 
+        def render_headers(headers)
+          2.times { @pdf.render_blank_line }
+          cursor = @pdf.cursor
+          headers.each_with_index do |header, index|
+            cursor -= 10 if index == 1
+            @pdf.bounding_box [columns[index][:position], cursor], width: columns[index][:width], height: 0.3.cm do
+              @pdf.text header[:content], header[:options]
+            end
+          end
+        end
+
         def headers
           [
-            header_column('Código', :left),
-            header_column('Descrição', :left),
-            header_column('Qtde', :right),
+            header_column('CÓDIGO - DESCRIÇÃO', :left),
+            header_column('QTD', :left),
             header_column('UN', :right),
-            header_column('Vl Unit', :right),
-            header_column('Vl Total', :right)
+            header_column('VL UNIT', :right),
+            header_column('TOTAL', :right)
           ]
         end
 
         def header_column(title, align)
-          { content: title, options: { align: align, size: 6, style: :bold } }
+          { content: title, options: { align: align, size: 9, style: :bold } }
+        end
+
+        def columns
+          [
+            { width: 6.7.cm, position: 0 },
+            { width: 1.7.cm, position: 0.cm },
+            { width: 1.cm, position: 1.7.cm },
+            { width: 1.7.cm, position: 2.7.cm },
+            { width: 2.3.cm, position: 4.4.cm }
+          ]
+        end
+
+        def render_products
+          @pdf.render_blank_line
+          products.each do |product|
+            cursor = @pdf.cursor
+
+            product.each_with_index do |cell, index|
+              cursor -= 10 if index == 1
+              @pdf.bounding_box [columns[index][:position], cursor], width: columns[index][:width], height: 0.3.cm do
+                options = cell[:options].merge(overflow: :truncate)
+                @pdf.text_box cell[:content], options
+              end
+            end
+          end
         end
 
         def products
@@ -35,9 +70,8 @@ module BrDanfe
 
         def product(det)
           [
-            cell_text(det.css('prod/cProd').text),
-            cell_text(det.css('prod/xProd').text),
-            cell_number(BrDanfe::Helper.numerify(det.css('prod/qCom').text)),
+            cell_text("#{det.css('prod/cProd').text} - #{det.css('prod/xProd').text}"),
+            cell_text(BrDanfe::Helper.numerify(det.css('prod/qCom').text), align: :left),
             cell_text(det.css('prod/uCom').text, align: :right),
             cell_number(BrDanfe::Helper.numerify(det.css('prod/vUnCom').text)),
             cell_number(BrDanfe::Helper.numerify(det.css('prod/vProd').text))
@@ -45,56 +79,13 @@ module BrDanfe
         end
 
         def cell_text(text, options = {})
-          cell = { content: text, options: { border_width: 0, size: 6 } }
+          cell = { content: text, options: { border_width: 0, size: 9 } }
           cell[:options].merge!(options)
           cell
         end
 
         def cell_number(text)
           cell_text(text, align: :right)
-        end
-
-        def columns
-          [
-            { width: 0.9.cm, position: 0 },
-            { width: 2.6.cm, position: 0.9.cm },
-            { width: 1.1.cm, position: 3.5.cm },
-            { width: 0.4.cm, position: 4.6.cm },
-            { width: 1.2.cm, position: 5.cm },
-            { width: 1.2.cm, position: 6.2.cm }
-          ]
-        end
-
-        def render_headers(headers)
-          2.times { @pdf.render_blank_line }
-          cursor = @pdf.cursor
-          headers.each_with_index do |header, index|
-            @pdf.bounding_box [columns[index][:position], cursor], width: columns[index][:width], height: 0.2.cm do
-              @pdf.text header[:content], header[:options]
-            end
-          end
-        end
-
-        def render_products
-          @pdf.render_blank_line
-          index_of_product_name = 1
-          products.each do |product|
-            box_height = box_height(product[index_of_product_name][:content])
-            cursor = @pdf.cursor
-
-            product.each_with_index do |cell, index|
-              @pdf.bounding_box [columns[index][:position], cursor], width: columns[index][:width], height: box_height do
-                @pdf.text cell[:content], cell[:options]
-              end
-            end
-          end
-        end
-
-        def box_height(content)
-          line_height_base = 0.23.cm
-
-          lines = content.scan(/[\s\S]{1,20}( |$)/).length
-          line_height_base * lines
         end
       end
     end
