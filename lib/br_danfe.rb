7ca89025059a34f5b9bca58cb7acfe5de2184488
Marcos de Melo <marcosdemelo.br@gmail.com>
2022-08-30 09:29:21 -0300

    Configure I18n gem (#180)

--- lib/br_danfe.rb
+++ lib/br_danfe.rb
@@ -11,7 +11,12 @@ require 'i18n'
 require 'br_documents'
 
 Dir["#{File.dirname(__FILE__)}/**/*.rb"].sort.each { |f| require f }
+
 I18n.load_path << File.expand_path('../config/locales/pt-BR.yml', __dir__)
+I18n.available_locales = ['pt-BR']
+I18n.default_locale = 'pt-BR'
+I18n.enforce_available_locales = false
+
 Prawn::Font::AFM.hide_m17n_warning = true
 
 module BrDanfe
