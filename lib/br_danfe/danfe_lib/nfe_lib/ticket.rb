dee745bf031cec7e9a85e0167ca88e96b6929df0
Matheus Carelli <61876540+MatCRL@users.noreply.github.com>
2024-12-03 15:46:29 -0300

    Add new fields in ticket (#236)

--- lib/br_danfe/danfe_lib/nfe_lib/ticket.rb
+++ lib/br_danfe/danfe_lib/nfe_lib/ticket.rb
@@ -8,12 +8,42 @@ module BrDanfe
         end
 
         def render
-          @pdf.ibox 0.85, 15.60, 0.75, 1.85, I18n.t('danfe.ticket.xNome', xNome: @xml['emit/xNome'])
+          render_additional_data
           @pdf.ibox 0.85, 3.85, 0.75, 2.7, I18n.t('danfe.ticket.received_at')
           @pdf.ibox 0.85, 11.75, 4.60, 2.7, I18n.t('danfe.ticket.receiver')
           @pdf.ibox 1.70, 4.00, 16.35, 1.85, '',
                     I18n.t('danfe.ticket.document', nNF: @xml['ide/nNF'], serie: @xml['ide/serie']),
                     align: :center, valign: :center
+        end
+
+        def render_additional_data
+          data = generate_additional_data
+          @pdf.ibox 0.85, 15.60, 0.75, 1.85, '', data, size: 6
+        end
+
+        def generate_additional_data
+          additional_data = []
+          additional_data.push(name_content)
+          additional_data.push("DESTINATÁRIO: #{recipient_content}")
+          additional_data.push("EMITIDA EM: #{emmited_at_content}")
+          additional_data.push("VALOR TOTAL DA NOTA: R$ #{total_value_content}")
+          additional_data.join(' * ')
+        end
+
+        def name_content
+          I18n.t('danfe.ticket.xNome', xNome: @xml['emit/xNome'])
+        end
+
+        def recipient_content
+          @xml['dest/xNome'].to_s
+        end
+
+        def total_value_content
+          BrDanfe::Helper.numerify(@xml['ICMSTot/vNF']).to_s
+        end
+
+        def emmited_at_content
+          @xml['ide/dhEmi'].present? ? Helper.format_date(@xml['ide/dhEmi']) : Helper.format_date(@xml['ide/dEmi'])
         end
       end
     end
--- spec/br_danfe/danfe_lib/nfe_lib/ticket_spec.rb
+++ spec/br_danfe/danfe_lib/nfe_lib/ticket_spec.rb
@@ -15,12 +15,22 @@ describe BrDanfe::DanfeLib::NfeLib::Ticket do
         <NFe xmlns="http://www.portalfiscal.inf.br/nfe">
           <infNFe Id="NFe25111012345678901234550020000134151000134151" versao="2.00">
             <ide>
-              <nNF>1</nNF>
-              <serie>1</serie>
+              <dhEmi>2024-10-04T10:57:54-03:00</dhEmi>
             </ide>
             <emit>
-              <xNome>Nome do Remetente Ltda</xNome>
+              <xNome>Empresa teste</xNome>
             </emit>
+            <dest>
+              <CNPJ>18191228000171</CNPJ>
+              <xNome>NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL</xNome>
+              <indIEDest>9</indIEDest>
+              <email>desenvolvedores@asseinfo.com.br</email>
+            </dest>
+            <total>
+              <ICMSTot>
+                <vNF>40.00</vNF>
+              </ICMSTot>
+            </total>
           </infNFe>
         </NFe>
       XML
@@ -37,6 +47,94 @@ describe BrDanfe::DanfeLib::NfeLib::Ticket do
       pdf.render_file output_pdf
 
       expect("#{base_dir}ticket#render.pdf").to have_same_content_of file: output_pdf
+    end
+
+    context 'when xml returns a date' do
+      describe 'with dhEmi' do
+        let(:xml) { BrDanfe::XML.new(xml_with_dhEmi) }
+
+        let(:xml_with_dhEmi) do
+          <<~XML
+            <NFe xmlns="http://www.portalfiscal.inf.br/nfe">
+              <infNFe Id="NFe25111012345678901234550020000134151000134151" versao="2.00">
+                <ide>
+                  <dhEmi>2024-10-04T10:57:54-03:00</dhEmi>
+                </ide>
+                <emit>
+                  <xNome>Empresa teste</xNome>
+                </emit>
+                <dest>
+                  <CNPJ>18191228000171</CNPJ>
+                  <xNome>NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL</xNome>
+                  <indIEDest>9</indIEDest>
+                  <email>desenvolvedores@asseinfo.com.br</email>
+                </dest>
+                <total>
+                  <ICMSTot>
+                    <vNF>40.00</vNF>
+                  </ICMSTot>
+                </total>
+              </infNFe>
+            </NFe>
+          XML
+        end
+
+        before do
+          subject.render
+          File.delete(output_pdf) if File.exist?(output_pdf)
+        end
+
+        it 'renders xml to the pdf' do
+          expect(File.exist?(output_pdf)).to be_falsey
+
+          pdf.render_file output_pdf
+
+          expect("#{base_dir}ticket#render_with_dhEmit.pdf").to have_same_content_of file: output_pdf
+        end
+      end
+
+      describe 'with dEmi' do
+        let(:xml) { BrDanfe::XML.new(xml_with_dEmi) }
+
+        let(:xml_with_dEmi) do
+          <<~XML
+            <NFe xmlns="http://www.portalfiscal.inf.br/nfe">
+              <infNFe Id="NFe25111012345678901234550020000134151000134151" versao="2.00">
+                <ide>
+                  <dEmi>2024-10-04</dEmi>
+                </ide>
+                <emit>
+                  <xNome>Empresa teste</xNome>
+                </emit>
+                <dest>
+                  <CNPJ>18191228000171</CNPJ>
+                  <xNome>NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL</xNome>
+                  <indIEDest>9</indIEDest>
+                  <email>desenvolvedores@asseinfo.com.br</email>
+                </dest>
+                <total>
+                  <ICMSTot>
+                    <vNF>40.00</vNF>
+                  </ICMSTot>
+                </total>
+              </infNFe>
+            </NFe>
+          XML
+        end
+
+        before do
+          subject.render
+          File.delete(output_pdf) if File.exist?(output_pdf)
+        end
+
+        it 'renders xml to the pdf' do
+          expect(File.exist?(output_pdf)).to be_falsey
+
+          pdf.render_file output_pdf
+
+          expect("#{base_dir}ticket#render_with_dEmit.pdf").to have_same_content_of file: output_pdf
+        end
+      end
     end
   end
 end
