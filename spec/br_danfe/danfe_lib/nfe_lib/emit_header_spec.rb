c8cce71c4c237004f6abbc21a45891beb64d50d6
MatCRL <mcarelli@edu.univali.br>
2025-12-09 11:28:53 -0300

    fix coverage

--- spec/br_danfe/danfe_lib/nfe_lib/emit_header_spec.rb
+++ spec/br_danfe/danfe_lib/nfe_lib/emit_header_spec.rb
@@ -114,5 +114,68 @@ describe BrDanfe::DanfeLib::NfeLib::EmitHeader do
         end
       end
     end
+
+    context 'when emitter has CPF instead of CNPJ' do
+      let(:xml_as_string) do
+        <<~XML
+          <nfeProc xmlns="http://www.portalfiscal.inf.br/nfe" versao="2.00">
+            <NFe xmlns="http://www.portalfiscal.inf.br/nfe">
+              <infNFe Id="NFe25111012345678901234550020000134151000134151" versao="2.00">
+                <ide>
+                  <tpNF>1</tpNF>
+                  <nNF>1</nNF>
+                  <serie>1</serie>
+                  <natOp>Vendas de producao do estabelecimento</natOp>
+                </ide>
+                <emit>
+                  <xNome>Nome do Remetente</xNome>
+                  <CPF>86893723008</CPF>
+                  <IE>526926313553</IE>
+                  <enderEmit>
+                    <xLgr>Rua do Remetente, Casa</xLgr>
+                    <nro>123</nro>
+                    <xBairro>Bairro do Remetente</xBairro>
+                    <xMun>SAO PAULO</xMun>
+                    <UF>SP</UF>
+                    <CEP>12345678</CEP>
+                    <fone>1112345678</fone>
+                    <email>foo@bar.com</email>
+                  </enderEmit>
+                </emit>
+                <det nItem="1">
+                  <prod>
+                    <detExport>
+                      <exportInd>
+                        <chNFe>41211285493849000105550020000027171182910239</chNFe>
+                      </exportInd>
+                    </detExport>
+                  </prod>
+                </det>
+              </infNFe>
+            </NFe>
+            <protNFe xmlns="http://www.portalfiscal.inf.br/nfe" versao="2.00">
+              <infProt Id="ID325110012866320">
+                <chNFe>25111012345678901234550020000134151000134151</chNFe>
+                <dhRecbto>2011-10-29T14:37:09</dhRecbto>
+                <nProt>325110012866320</nProt>
+              </infProt>
+            </protNFe>
+          </nfeProc>
+        XML
+      end
+
+      before do
+        subject.render 1, 3.96, 1
+        FileUtils.rm_f(output_pdf)
+      end
+
+      it 'renders xml with CPF to the pdf' do
+        expect(File.exist?(output_pdf)).to be_falsey
+
+        pdf.render_file output_pdf
+
+        expect("#{base_dir}emit_header#render-with_cpf.pdf").to have_same_content_of file: output_pdf
+      end
+    end
   end
 end
