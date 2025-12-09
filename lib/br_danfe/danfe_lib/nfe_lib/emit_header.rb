44d62a0bb4944617df2abb0a79842e25c8afbd99
MatCRL <mcarelli@edu.univali.br>
2025-12-09 16:04:37 -0300

    fix position

--- lib/br_danfe/danfe_lib/nfe_lib/emit_header.rb
+++ lib/br_danfe/danfe_lib/nfe_lib/emit_header.rb
@@ -52,14 +52,6 @@ module BrDanfe
           BrDanfe::Helper.format_cep(@xml['enderEmit/CEP'])
         end
 
-        def cnpj_cpf
-          if @xml['emit/CNPJ'].present?
-            'emit/CNPJ'
-          else
-            'emit/CPF'
-          end
-        end
-
         def logo
           bounding_box_size = 80
           logo_options = BrDanfe::Logo::Options.new(bounding_box_size, @logo_dimensions).options
@@ -117,6 +109,14 @@ module BrDanfe
           @pdf.lie LINE_HEIGHT, 6.36, 0.75, y_second_line, @xml, 'enderEmit/UF', 'emit/IE'
           @pdf.lie LINE_HEIGHT, 6.86, 7.11, y_second_line, @xml, 'enderDest/UF', 'emit/IEST'
           @pdf.lcnpj_cpf LINE_HEIGHT, 6.34, 13.97, y_second_line, @xml, cnpj_cpf
+        end
+
+        def cnpj_cpf
+          if @xml['emit/CNPJ'].present?
+            'emit/CNPJ'
+          else
+            'emit/CPF'
+          end
         end
       end
     end
